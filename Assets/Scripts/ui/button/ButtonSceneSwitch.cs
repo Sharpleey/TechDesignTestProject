e3c446398e9f186f18a00d5436fdcc2646fb854c
Maxim Kirichenko <maksimkirichenko98@gmail.com>
2023-08-06 18:31:14 +0300

    На сценах main_scene сделал появление кнопки при на клике на сундук. Кнопка переводит на сцену level_1. На сцене level_1 аналогично разместил сундук и кнопку, которая переводит на сцену main_scene
    - Для кнопки реализовал скрипт ButtonSwitchScene.
    - Из кнопки сделал префаб и поместил в Prefabs\ui\buttons
    - Клик и выделение кнопки реализованы с использованием EventSystems
    - Анимации с изменением масштаба текста кнопки и изменения ее цвета сделана с помощью DoTween

---
+++ Assets/Scripts/ui/button/ButtonSceneSwitch.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+using Zenject;
+
+public class ButtonSceneSwitch : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    [SerializeField] GameSceneManager.Scene _switchScene;
+
+    [Space(10)]
+    [SerializeField] private Color _colorSellectedButton;
+    [SerializeField] private Color _colorSellectedTextButton;
+    [SerializeField] private Vector3 _scaleTextSellectedButton;
+
+    [SerializeField] private Ease easeScaleText;
+    [SerializeField] private Ease easeChangeColor;
+
+    private Image _backgroundButton;
+    private TextMeshProUGUI _textButton;
+    private Color _defaultColorButton;
+    private Color _defaultColorTextButton;
+    private Vector3 _defaultScaleTextButton;
+
+    private GameSceneManager _gameSceneManager;
+
+    [Inject]
+    private void Construct(GameSceneManager gameSceneManager)
+    {
+        _gameSceneManager = gameSceneManager;
+    }
+
+    private void Start()
+    {
+        _backgroundButton = GetComponent<Image>();
+        _textButton = GetComponentInChildren<TextMeshProUGUI>();
+
+        _defaultColorButton = _backgroundButton.color;
+        _defaultScaleTextButton = _textButton.transform.localScale;
+        _defaultColorTextButton = _textButton.color;
+    }
+
+    private void Select()
+    {
+        _backgroundButton.DOColor(_colorSellectedButton, 0.15f).SetEase(easeChangeColor);
+        _textButton.transform.DOScale(_scaleTextSellectedButton, 0.15f).SetEase(easeScaleText);
+        _textButton.DOColor(_colorSellectedTextButton, 0.15f).SetEase(easeChangeColor);
+    }
+
+    private void Deselect()
+    {
+        _backgroundButton.DOColor(_defaultColorButton, 0.15f).SetEase(easeChangeColor);
+        _textButton.transform.DOScale(_defaultScaleTextButton, 0.15f).SetEase(easeScaleText);
+        _textButton.DOColor(_defaultColorTextButton, 0.15f).SetEase(easeChangeColor);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Deselect();
+
+        _gameSceneManager?.SwitchToScene(_switchScene);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Select();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {   
+        Deselect();
+    }
+
+}
